8b62ee6fff3ce67ce839f1ed2ab89430138d86a9
Truong Giang <giangtqvn01@gmail.com>
2021-09-10 01:35:53 +0700

    ngfor comboBox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,11 @@ export class HomeComponent implements OnInit {
   public  name ='Huy nguyễn';
   public age = 15;
   public traiCay =['Táo', "Cam"];
-  public traiCay2 =[{ten : 'Táo', gia:12 , haGia:false},{ten : 'Quýt', gia:-13,haGia:true}]
+  public traiCay2 =[{ten : 'Táo', gia:12 , haGia:false},{ten : 'Quýt', gia:-13,haGia:true}];
+  public vietNamData=[{city:"Tỉnh/Thành phố",district:["Quận/Huyện"]},{city:"Hà Nội", district:["Hà Đông"]},
+  {city:"Tuyên Quang", district:["Thành phố Tuyên Quang"]}
+  ];
+  public district:string[]=["Quận/Huyện"];
 
   constructor() { }
 
@@ -21,5 +25,18 @@ export class HomeComponent implements OnInit {
   public resetName() : void{
     console.log('resetName');
     this.name='';
+  }
+  public changeCity(event:any):void{
+    const city = event.target.value;
+    if(!city){
+      return;
+    }
+    // const search = this.vietNamData.filter(data => data.city === city);
+    // console.log("search", search);
+    // if(search && search.length >0){
+    //   this.district = search[0].district;
+    // }
+
+    this.district = this.vietNamData.find(data => data.city === city)?.district || [];
   }
 }
